3fcd424a1b39edfc7c1b1aaa489e78bcbd12ffaf
Ingar Helgesen <ingar@i-h.no>
2021-02-21 16:06:59 +0100

    Added JSDoc comments to types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -27,40 +27,58 @@ export class InvalidFractionIdInResponseError extends Error {
 // Client Types
 
 export interface MinRenovasjonClientOptions {
+  /** An appKey is required, you can get one by requesting it here: https://www.norkart.no/product/api-for-utviklere/ */
   appKey: string
+  /** The base URL for the Norkart API. Default: 'https://komteksky.norkart.no/komtek.renovasjonwebapi/api' */
   norkartBaseUrl?: string
+  /** The base URL for the Geonorge API. Default: 'https://ws.geonorge.no/adresser/v1' */
   geonorgeBaseUrl?: string
 }
 
 export interface MinRenovasjonGetFractionsOptions {
+  /** The code for the wanted municipality, get one from the `.getAddressInfo(options)` method. AKA "Kommunenummer" */
   municipalityNumber: string
 }
 
 export interface MinRenovasjonFractions {
+  /** The ID for this fraction */
   id: number
+  /** The name for this fraction */
   name: string
+  /** The icon URL for this fraction */
   iconUrl: string
 }
 
 export interface MinRenovasjonEmptyingDates {
+  /** The ID for the fraction of these emptying dates */
   fractionId: number
+  /** The name for the fraction of these emptying dates */
   fraction: string
+  /** The icon URL for the fraction of these emptying dates */
   fractionIconUrl: string
+  /** The emptying dates as a JS Date object */
   dates: Date[]
 }
 
 export interface MinRenovasjonGetEmptyingDatesOptions {
+  /** The code for the wanted municipality, get one from the `.getAddressInfo(options)` method. AKA "Kommunenummer". Ex. `Skien` = `3807` */
   municipalityNumber: string
+  /** A specific code for the address (addressName), get one from the `.getAddressInfo(options)` method. AKA "Adressekode". Ex. `Fylkesbakken, Skien` = `31700` */
   addressCode: string
+  /** The streetname without the housenumber. Ex. `Fylkesbakken` */
   addressName: string
+  /** A house's number on the specified street, can include the houseletter too. Ex. `23A` */
   houseNumber: string
 }
 
 export type MinRenovasjonGetAddressInfo = MinRenovasjonGetEmptyingDatesOptions
 
 export interface MinRenovasjonGetAddressInfoOptions {
+  /** The streetname without the housenumber. Ex. `Fylkesbakken` */
   addressName: string
+  /** A house's number on the specified street, can include the houseletter too. Ex. `23A` */
   houseNumber: string
+  /** The municipality to search for the address in. AKA "Kommune". Ex. `Skien` */
   municipality: string
 }
 
