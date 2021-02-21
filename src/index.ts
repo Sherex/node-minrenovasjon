32f8a83f0f80b179a1800bac8bd98868427bc9ea
Ingar Helgesen <ingar@i-h.no>
2021-02-21 09:42:36 +0100

    Better error-handling and typings

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosInstance } from 'axios'
 import {
   MissingParameterError,
+  UnexpectedAPIResponseError,
   MinRenovasjonClientOptions,
   MinRenovasjonGetFractionsOptions,
-  MinRenovasjonFractions
+  MinRenovasjonFractions,
+  _GetFractionsRawResponse,
+  isGetFractionRawResponse
 } from './lib/types'
 
 export class MinRenovasjonClient {
@@ -22,11 +25,22 @@ export class MinRenovasjonClient {
 
   async getFractions (options: MinRenovasjonGetFractionsOptions): Promise<MinRenovasjonFractions[]> {
     if (typeof options.municipalityNumber !== 'string') throw new MissingParameterError('municipalityNumber')
-    const response = await this._api.get('fraksjoner', {
+    const response = await this._api.get<_GetFractionsRawResponse[]>('fraksjoner', {
       headers: {
         Kommunenr: options.municipalityNumber
       }
     })
-    return response.data
+
+    if (!Array.isArray(response.data) || response.data.map(isGetFractionRawResponse).includes(false)) {
+      throw new UnexpectedAPIResponseError(response)
+    }
+
+    const formattedData: MinRenovasjonFractions[] = response.data.map(entry => ({
+      id: entry.Id,
+      name: entry.Navn,
+      iconUrl: entry.Ikon
+    }))
+
+    return formattedData
   }
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,24 @@
+import { AxiosResponse } from 'axios'
+
 export class MissingParameterError extends Error {
   constructor (parameter: string) {
     super(`Required parameter "${parameter}" is missing or of the wrong type!`)
   }
 }
+
+export class UnexpectedAPIResponseError extends Error {
+  readonly axiosResponse: AxiosResponse
+  constructor (response: AxiosResponse, url?: string) {
+    if (typeof response.config.baseURL === 'string' && typeof response.config.url === 'string') {
+      const separator = response.config.baseURL.endsWith('/') ? '' : '/'
+      url = url ?? `${response.config.baseURL}${separator}${response.config.url}`
+    }
+    super(`Received an unexpected response from "${url ?? 'Invalid URL'}"! More details in "error.axiosResponse"`)
+    this.axiosResponse = response
+  }
+}
+
+// Client Types
 
 export interface MinRenovasjonClientOptions {
   appKey: string
@@ -30,4 +46,19 @@ export interface MinRenovasjonGetEmptyingDatesOptions {
   streetCode: string
   streetName: string
   houseNumber: string
+}
+
+// API Types
+
+export interface _GetFractionsRawResponse {
+  Id: number
+  Navn: string
+  Ikon: string
+}
+
+export function isGetFractionRawResponse (data: any): data is _GetFractionsRawResponse {
+  if (typeof data.Id !== 'number') return false
+  if (typeof data.Navn !== 'string') return false
+  if (typeof data.Ikon !== 'string') return false
+  return true
 }
